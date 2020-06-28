a90756388d08a9a6cabc15df7d41f754776a40a4
Achim Friedland <github@ahzf.de>
2020-06-29 01:06:42 +0200

    Fix charge point scripts

--- documentation/ChargePoint/Testdata-secp256r1/sign.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "$1 -> $1.sign using private key file $2"
-openssl dgst -sha256 -sign $2 $1 > $1.sign
--- documentation/ChargePoint/Testdata-secp256r1/tar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Compressing $1 -> $1.tar.bz2"
-cd $1
-tar -cjf ../$1.tar.bz2 *
-cd ..
