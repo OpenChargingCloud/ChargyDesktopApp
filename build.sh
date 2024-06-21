d2a66240ab2f7bb55137f7964de9fef57f11d9b9
Achim Friedland <achim.friedland@graphdefined.com>
2024-06-21 02:15:02 +0200

    Work around still unfixed dependency problem

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://github.com/Automattic/node-canvas/issues/2375
+rm node_modules/canvas -rf
+
 #tsc -p tsconfig.json
 sass src/css/chargy.scss src/css/chargy.css
 webpack -c webpack.config.cjs
