5d07c68f0c0d25dfc3e0ad8bef6e61cec3f383c0
Achim Friedland <achim.friedland@graphdefined.com>
2024-11-21 00:41:42 +0100

    Another small fix

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # https://github.com/Automattic/node-canvas/issues/2375
 rm node_modules/canvas -rf
 
+npm run sbom
 #tsc -p tsconfig.json
 sass src/css/chargy.scss src/css/chargy.css
 webpack -c webpack.config.cjs
